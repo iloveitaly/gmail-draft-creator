72d8a34ae4f0ab1aad6a39dd0cdfaaf47e92ce98
Michael Bianco <iloveitaly@gmail.com>
2023-07-02 06:26:01 -0600

    drafting in gmail is working

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
-from decouple import config
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
 from googleapiclient.discovery import build
 from email.mime.text import MIMEText
+import os
 import base64
+import pickle
 
 SCOPES = ['https://www.googleapis.com/auth/gmail.compose']
 
 def create_draft(email, name, subject, template):
-    creds = config('GMAIL_API_TOKEN')
+    creds = None
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
 
-    service = build('gmail', 'v1', developerKey=creds)
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('gmail', 'v1', credentials=creds)
 
     message = MIMEText(template)
     message['to'] = email
@@ -16,6 +32,8 @@ def create_draft(email, name, subject, template):
 
     raw_message = base64.urlsafe_b64encode(message.as_bytes())
     raw_message = raw_message.decode()
-    body = {'raw': raw_message}
+    body = {'message': {'raw': raw_message}}
 
     draft = service.users().drafts().create(userId='me', body=body).execute()
+
+create_draft('iloveitaly@gmail.com', 'Mike', 'Test', 'Hello, World!')
--- readme.md
+++ readme.md
@@ -1,7 +1,18 @@
-1. Navigate to the Google API Console: `https://console.developers.google.com/`
-2. If you don't already have a project, create a new one.
-3. Enable the Gmail API for your project.
-4. Click on "Credentials" in the left-hand side menu.
-5. Click "Create Credentials" and choose "API Key".
-6. After your API Key is created, copy the key value.
-7. Store this key value as `GMAIL_API_TOKEN` in your `.env` file or your operating system's environment variables.
+
+## Generating a Gmail API Token
+
+1. Navigate to the Google Cloud Console. https://console.developers.google.com/
+2. Create a new project or select an existing one.
+3. Go to "APIs & Services" -> "Library" and enable the Gmail API.
+4. Navigate to "APIs & Services" -> "Credentials".
+5. Click "Create Credentials" -> "OAuth client ID".
+6. Select "Desktop app" as the application type, then click "Create".
+7. Download the JSON file, rename it to `credentials.json`, and place it in the root of your project.
+8. Run the script and oauth into your account
+
+### Scopes Needed
+
+* https://www.googleapis.com/auth/gmail.compose
+* https://www.googleapis.com/auth/gmail.readonly
+* https://www.googleapis.com/auth/calendar.readonly
+* https://www.googleapis.com/auth/calendar.event
