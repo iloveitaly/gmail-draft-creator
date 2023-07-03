99d28127387f6508ddb0d63177ba6045f6fc7df2
Michael Bianco <iloveitaly@gmail.com>
2023-07-03 11:30:37 -0600

    markdown and better cli structure

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ import os
 import base64
 import pickle
 import csv
+import logging
+import markdown
+from email.mime.multipart import MIMEMultipart
+import re
 
 SCOPES = [
     'https://www.googleapis.com/auth/gmail.compose',
@@ -19,22 +23,23 @@ SCOPES = [
 import click
 
 @click.command()
-@click.argument('csv_file_path', type=click.Path(exists=True))
-@click.argument('template_file_path', type=click.Path(exists=True))
-@click.argument('subject', type=str)
+@click.option('--csv', 'csv_file_path', type=click.Path(exists=True), help='Path to the CSV file.', required=True)
+@click.option('--template', 'template_file_path', type=click.Path(exists=True), help='Path to the template file.', required=True)
+@click.option('--subject', 'subject', type=str, help='Subject for the email drafts.', required=True)
 @click.option('--dry-run', is_flag=True, default=False, help='Run script without creating drafts.')
 def send_drafts_from_csv_cli(csv_file_path, template_file_path, subject, dry_run):
     send_drafts_from_csv(csv_file_path, template_file_path, subject, dry_run)
 
 def send_drafts_from_csv(csv_file_path, template_file_path, subject, dry_run=False):
     with open(template_file_path, 'r') as template_file:
-        template_string = template_file.read()
+        template_string = markdown.markdown(template_file.read())
 
     with open(csv_file_path, 'r') as file:
         reader = csv.DictReader(file)
         for row in reader:
             email = row.pop('email', None) or row.pop('Email', None)
             template_params = {k.lower(): v for k, v in row.items()}
+
             if email is not None:
                 create_draft(email, subject, template_string, template_params, dry_run)
 
@@ -58,9 +63,16 @@ def create_draft(email, subject, template_string, template_params, dry_run=False
     message_template = Template(template_string)
     message_text = message_template.substitute(template_params)
 
-    message = MIMEText(message_text)
+    # remove <p> and </p> tags if they are on the first line
+    # this causes weird formatting in gmail
+    first_line, remaining_text = message_text.split('\n', 1)
+    first_line = re.sub(r"^<p>(.*)</p>$", r"\1", first_line)
+    message_text = first_line + '\n' + remaining_text
+
+    message = MIMEMultipart('alternative')
     message['to'] = email
     message['subject'] = subject
+    message.attach(MIMEText(message_text, 'html'))
 
     raw_message = base64.urlsafe_b64encode(message.as_bytes())
     raw_message = raw_message.decode()
@@ -69,6 +81,9 @@ def create_draft(email, subject, template_string, template_params, dry_run=False
     if not dry_run:
         draft = service.users().drafts().create(userId='me', body=body).execute()
 
+    logging.info(f'draft created for email: {email}')
+
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
     send_drafts_from_csv_cli()
